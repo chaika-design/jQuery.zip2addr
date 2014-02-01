14be5f966fb83bef95e62ed2964bc2aa06e2969c
Chaika <chaika.design@gmail.com>
2014-02-02 04:39:46 +0900

    リファクタリング

--- jquery.zip2addr.js
+++ jquery.zip2addr.js
@@ -9,7 +9,6 @@
  * Depends:
  *  jQuery 1.4 or above
  */
-
 $.fn.zip2addr = function(target){
   var c = {
     api: location.protocol + '//www.google.com/transliterate?langpair=ja-Hira|ja&jsonp=?',
@@ -21,41 +20,46 @@ $.fn.zip2addr = function(target){
 
   var getAddr = function (zip,callback) {
     $.getJSON(c.api,{'text':zip},
-      function(json){
+      function(json) {
         if(RegExp(c.prefectureToken).test(json[0][1][0])){
-          callback(json[0][1][0].replace(RegExp('(.*?'+c.prefectureToken+')(.+)'),function(a,b,c,d){return [b,d]}))
+          callback(json[0][1][0].replace(RegExp('(.*?'+c.prefectureToken+')(.+)'), function(a,b,c,d){return [b,d];}));
         }
       }
-    )
-  }
+    );
+  };
 
-  var fillAddr = (function(){
-    if(typeof target == 'object' && target.pref){
+  var fillAddr = (function() {
+    if(typeof target == 'object' && target.pref) {
+      var $addr = $(target.addr);
+      var $pref = $(target.pref);
       return function(addr){
         var addrs = addr.split(',');
+        var value = $addr.val();
         if(addrs){
-          if(!RegExp(addrs[1]).test($(target.addr).val())){
-            $(target.pref).val(addrs[0]);
-            $(target.addr).val(addrs[1]);
+          if( !RegExp(addrs[1]).test(value) ) {
+            $pref.val(addrs[0]);
+            $addr.val(addrs[1]);
           }
-        }else if(!RegExp(addrs[1]).test($(target.addr).val())){
-          $(target.pref).add(target.addr).val('');
+        } else if(!RegExp(addrs[1]).test( value )) {
+          $pref.add(target.addr).val('');
         }
-      }
-    }else{
-      return function(addr){
+      };
+    } else {
+      return function(addr) {
         var addrStr = addr.replace(',','');
         var addrField = target.addr || target;
+        var $addrField = $(addrField);
+        var value = $addrField.val();
         if(addrStr){
-          if(!RegExp(addrStr).test($(addrField).val())){
-            $(addrField).val(addrStr);
+          if(!RegExp(addrStr).test(value)) {
+            $addrField.val(addrStr);
           }
-        }else if(!RegExp(addrStr).test($(addrField).val())){
-          $(addrField).val('');
+        } else if(!RegExp(addrStr).test(value)) {
+          $addrField.val('');
         }
-      }
+      };
     }
-  })()
+  })();
 
   //From http://liosk.blog103.fc2.com/blog-entry-72.html
   var fascii2ascii = (function() {
@@ -65,46 +69,43 @@ $.fn.zip2addr = function(target){
     return function(s){return s.replace(pattern, replace);};
   })();
 
-  var check = function(_val){
+  var check = function(_val) {
     var val = fascii2ascii(_val).replace(/\D/g,'');
-    if(val.length == 7){
-      if(cache[val] == undefined){
-        getAddr(val.replace(/(\d\d\d)(\d\d\d\d)/,'$1-$2'),function(json){
+    if(val.length == 7) {
+      if(cache[val] == undefined) {
+        getAddr(val.replace(/(\d\d\d)(\d\d\d\d)/,'$1-$2'),function(json) {
           cache[val] = json;
           fillAddr(json);
-        })
-      }else{
+        });
+      } else {
         fillAddr(cache[val]);
       }
     }
-  }
+  };
 
-    this.each(function(){
-        var elem = $(this);
-    if(typeof target == 'object' && target.zip2){
-      elem.add($(target.zip2))
-        .bind('keyup.zip2addr change.zip2addr',function(){
-          check(elem.val()+''+$(target.zip2).val())
-        })
-        .bind('blur.zip2addr',function(){
-          $(this).val(function(){
-            return fascii2ascii($(this).val())
-          })
-        })
-    }else{
-      elem
-        .bind('keyup.zip2addr change.zip2addr',function(){
-          check(elem.val())
-        })
-        .bind('blur.zip2addr',function(){
-          $(this).val(function(){
-            return fascii2ascii($(this).val()).replace(/\D/g,'').replace(/(\d\d\d)(\d\d\d\d)/,'$1'+c.zipDelimiter+'$2')
-          })
-        })
+  this.each(function() {
+    var $t = $(this);
+    if(typeof target == 'object' && target.zip2) {
+      var $zip2 = $(target.zip2);
+      $t.add($zip2).on('keyup.zip2addr change.zip2addr', function() {
+        check($t.val()+''+$zip2.val());
+      }).on('blur.zip2addr', function() {
+        $(this).val(function() {
+          return fascii2ascii($(this).val());
+        });
+      });
+    } else {
+      $t.on('keyup.zip2addr change.zip2addr', function() {
+        check($t.val());
+      }).on('blur.zip2addr',function(){
+        $t.val(function(){
+          return fascii2ascii($t.val()).replace(/\D/g,'').replace(/(\d\d\d)(\d\d\d\d)/,'$1'+c.zipDelimiter+'$2');
+        });
+      });
     }
-    });
+  });
 
-    return this;
+  return this;
 };
 
 $.fn.zip2addr.cache = {};
